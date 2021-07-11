97ff328301aeca398ade2e79fb681d09939c85b9
Paulo <pauloeduardo2709@gmail.com>
2021-07-11 20:14:37 -0300

    feat: add dev prefix

--- prefix/text.go
+++ prefix/text.go
@@ -74,6 +74,10 @@ var TextPrefix = PrefixPack{
 			Name:        "i18n",
 		},
 		{
+			Description: "add/remove development related stuff",
+			Name:        "dev",
+		},
+		{
 			Description: "work in progress",
 			Name:        "wip",
 		},
