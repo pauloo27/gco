f3a37033fc6b030dc83b8ebd76e5e68a361feb53
Paulo <pauloeduardo2709@gmail.com>
2021-07-26 18:06:16 -0300

    feat: add simple "selection instruction" parser
    it's not fully working, some tests are not passing
    [ci skip]

---
+++ utils/git/add.go
@@ -0,0 +1,90 @@
+package git
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/Pauloo27/gco/utils"
+	"github.com/c-bata/go-prompt"
+)
+
+func parseInstruction(instruction string, filesMap map[string]bool, files []string) (map[string]bool, error) {
+	newFilesMap := filesMap
+	if instruction == "*" {
+		for file := range newFilesMap {
+			newFilesMap[file] = true
+		}
+		return newFilesMap, nil
+	}
+	splittedString := strings.Split(instruction, "-")
+	if len(splittedString) == 1 {
+		entry := splittedString[0]
+		// not a range
+		value := true
+		if strings.HasPrefix(splittedString[0], "^") {
+			entry = strings.TrimPrefix(entry, "^")
+			value = false
+		}
+		number, err := strconv.Atoi(entry)
+		if err != nil {
+			return newFilesMap, err
+		}
+		newFilesMap[files[number-1]] = value
+		return newFilesMap, nil
+	} else if len(splittedString) == 2 {
+		// range
+		startStr, endStr := splittedString[0], splittedString[1]
+		start, err := strconv.Atoi(startStr)
+		if err != nil {
+			return newFilesMap, err
+		}
+		end, err := strconv.Atoi(endStr)
+		if err != nil {
+			return newFilesMap, err
+		}
+		value := true
+		for i := start; i <= end; i++ {
+			newFilesMap[files[i-1]] = value
+		}
+		return newFilesMap, nil
+	}
+	return newFilesMap, errors.New("Invalid instruction")
+}
+
+func PromptFilesToAdd(out prompt.ConsoleWriter, filesName []string) {
+	for {
+		input := utils.Prompt(
+			" Files to commit (enter ? for help) ", utils.IndexedCompleter(filesName),
+		)
+
+		if input == "?" {
+			utils.PrettyPrint(out, "To commit files that are already added: ",
+				prompt.Blue, "press enter", prompt.DefaultColor, "\n",
+			)
+			utils.PrettyPrint(out, "To select files 1 and 3: ", prompt.Blue,
+				"1 3", prompt.DefaultColor, "\n",
+			)
+			utils.PrettyPrint(out,
+				"To select files 1, 2, 3 and 4: ", prompt.Blue, "1-4", prompt.DefaultColor,
+				" (", prompt.Blue, "1 2 3 4", prompt.DefaultColor, " also works)\n",
+			)
+			utils.PrettyPrint(out, "To select all files: ", prompt.Blue, "*",
+				prompt.DefaultColor, "\n",
+			)
+			utils.PrettyPrint(out, "To select all files expect 3: ", prompt.Blue,
+				"^3", prompt.DefaultColor, "\n",
+			)
+			continue
+		}
+
+		filesToCommit := strings.Split(input, " ")
+		if len(filesToCommit) != 0 {
+			for _, file := range filesToCommit {
+				fmt.Println(file)
+			}
+		}
+		break
+	}
+}
---
+++ utils/git/add_test.go
@@ -0,0 +1,85 @@
+package git
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func assertFiles(t *testing.T, rawFiles []string, expectedIndexes []int, result map[string]bool) {
+	shouldBeAdded := func(i int) bool {
+		for _, value := range expectedIndexes {
+			if value-1 == i {
+				return true
+			}
+		}
+		return false
+	}
+
+	for i, file := range rawFiles {
+		if result[file] != shouldBeAdded(i) {
+			t.Fatalf("File %s was %v\nResult: %v.", file, result[file], result)
+			return
+		}
+	}
+}
+
+func TestParseInstructions(t *testing.T) {
+	rawFiles := []string{
+		// 1					2									3									4
+		"hello.txt", "secrets.sqlite", "passwords.txt", "wifi-cracker.js",
+		// 5								6
+		"despacito-2.mp3", "marvel-movie-about-super-heros.mp4",
+	}
+
+	getFilesMap := func(initialValue bool) (map[string]bool, []int) {
+		filesMap := map[string]bool{}
+
+		allIndexes := []int{}
+
+		for i, file := range rawFiles {
+			filesMap[file] = initialValue
+			allIndexes = append(allIndexes, i+1)
+		}
+		return filesMap, allIndexes
+	}
+
+	t.Run("Simple number", func(t *testing.T) {
+		filesMap, _ := getFilesMap(false)
+		newMap, err := parseInstruction("1", filesMap, rawFiles)
+		assert.Nil(t, err)
+		assertFiles(t, rawFiles, []int{1}, newMap)
+	})
+	t.Run("Star", func(t *testing.T) {
+		filesMap, allIndexes := getFilesMap(false)
+		newMap, err := parseInstruction("*", filesMap, rawFiles)
+		assert.Nil(t, err)
+		assertFiles(t, rawFiles, allIndexes, newMap)
+	})
+	t.Run("Range", func(t *testing.T) {
+		filesMap, _ := getFilesMap(false)
+		newMap, err := parseInstruction("2-5", filesMap, rawFiles)
+		assert.Nil(t, err)
+		assertFiles(t, rawFiles, []int{2, 3, 4, 5}, newMap)
+	})
+	t.Run("Negate", func(t *testing.T) {
+		filesMap, _ := getFilesMap(true)
+		newMap, err := parseInstruction("^5", filesMap, rawFiles)
+		assert.Nil(t, err)
+		assertFiles(t, rawFiles, []int{1, 2, 3, 4, 6}, newMap)
+	})
+	t.Run("Negate range", func(t *testing.T) {
+		filesMap, _ := getFilesMap(false)
+		newMap, err := parseInstruction("^2-5", filesMap, rawFiles)
+		assert.Nil(t, err)
+		assertFiles(t, rawFiles, []int{1, 6}, newMap)
+	})
+	// TODO: instruction list
+	// TODO: index out of bounds (negative and 0 as well)
+	// TODO: invalid syntax: empty string
+	// TODO: invalid syntax: multiple negates
+	// TODO: invalid syntax: NaN
+	// TODO: invalid syntax: invalid range (start after the end)
+	// TODO: invalid syntax: invalid range (missing parts)
+	// TODO: invalid syntax: invalid range (multiple -)
+}
