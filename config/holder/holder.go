ff43ebe54f60db36793762889d77ad79c5f47362
Paulo <pauloeduardo2709@gmail.com>
2021-07-28 02:06:34 -0300

    feat: add support to global config

--- config/holder/holder.go
+++ config/holder/holder.go
@@ -11,11 +11,18 @@ var defaultConfig = &config.Config{
 	PrefixPack: "text",
 }
 
-var globalConfig *config.Config
-var projectConfig *config.Config
-var projectConfigNotFound bool
+var globalConfig = &config.Config{}
+var projectConfig = &config.Config{}
 
 var rcFileName = ".gco.json"
+
+func StoreGlobalConfig(conf *config.Config) error {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+	return config.StoreConfig(conf, home+"/"+rcFileName)
+}
 
 func StoreProjectConfig(conf *config.Config) error {
 	gitRoot, err := git.GetRepositoryRoot()
@@ -25,22 +32,43 @@ func StoreProjectConfig(conf *config.Config) error {
 	return config.StoreConfig(conf, gitRoot+"/"+rcFileName)
 }
 
-func GetProjectConfig() (*config.Config, error) {
-	if projectConfig == nil && !projectConfigNotFound {
-		var err error
-		gitRoot, err := git.GetRepositoryRoot()
-		if err != nil {
-			return nil, err
-		}
-		projectConfig, err = config.LoadConfig(gitRoot + "/" + rcFileName)
-		if err != nil {
-			if os.IsNotExist(err) {
-				projectConfigNotFound = true
-				return nil, err
-			} else {
-				return nil, err
-			}
-		}
+func GetGlobalConfig() (*config.Config, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
 	}
-	return projectConfig, nil
+	err = loadConfig(home+"/"+rcFileName, globalConfig)
+	return globalConfig, err
+}
+
+func GetProjectConfig() (*config.Config, error) {
+	gitRoot, err := git.GetRepositoryRoot()
+	if err != nil {
+		return nil, err
+	}
+	err = loadConfig(gitRoot+"/"+rcFileName, projectConfig)
+	return projectConfig, err
+}
+
+func GetProjectConfigOrGlobal() (conf *config.Config, err error, isFromProject bool) {
+	conf, err = GetProjectConfig()
+	if err == nil {
+		isFromProject = true
+		return
+	}
+
+	if os.IsNotExist(err) {
+		conf, err = GetGlobalConfig()
+	}
+
+	return
+}
+
+func loadConfig(path string, conf *config.Config) error {
+	loadedConf, err := config.LoadConfig(path)
+	if err != nil {
+		return err
+	}
+	*conf = *loadedConf
+	return nil
 }
